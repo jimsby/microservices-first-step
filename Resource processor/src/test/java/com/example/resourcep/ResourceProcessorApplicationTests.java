b06b7f597b7b5e38ad2e0e1507a721f3db7ebc4c
Ilya_Skumin <jimsby@gmail.com>
2022-04-02 15:16:07 +0300

    1.2 Create resource processor module

---
+++ Resource processor/src/test/java/com/example/resourcep/ResourceProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourcep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
