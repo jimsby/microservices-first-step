7eb3bd2fdad0b47c69462adf786cb0117d23d0a8
Ilya_Skumin <jimsby@gmail.com>
2022-04-25 15:33:46 +0300

    6.2 Use Resilience4j Circuit Breaker with Spring Cache (at OpenFeign).

--- resource-service/src/main/java/com/ms/resource/ResourceServiceApplication.java
+++ resource-service/src/main/java/com/ms/resource/ResourceServiceApplication.java
@@ -2,11 +2,13 @@ package com.ms.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCaching
 public class ResourceServiceApplication {
 
     public static void main(String[] args) {
--- resource-service/src/main/java/com/ms/resource/service/StorageService.java
+++ resource-service/src/main/java/com/ms/resource/service/StorageService.java
@@ -1,8 +1,12 @@
 package com.ms.resource.service;
 
 import com.ms.resource.dto.StorageDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Profile;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -11,6 +15,19 @@ import java.util.List;
 @FeignClient(name = "${storageservice.name}", url = "${gateway.url}", primary = false)
 public interface StorageService {
 
+    @CachePut("storages")
     @GetMapping("/storages")
+    @CircuitBreaker(name = "backendA", fallbackMethod = "fallback")
     List<StorageDto> getStoragesList();
+
+
+    default List<StorageDto> fallback(Exception e) {
+        return getCached();
+    }
+
+    @Cacheable("storages")
+    default List<StorageDto> getCached() {
+        return getStoragesList();
+    }
+
 }
